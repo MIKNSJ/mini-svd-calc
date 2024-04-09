818f75c2c16052331094bd67093073f84dbd06b4
1 <95945513+MIKNSJ@users.noreply.github.com>
2024-04-09 00:07:24 -0700

    construct done and input init (#1)

---
+++ src/input.cpp
@@ -0,0 +1,13 @@
+// ===== Established 2024-04-08 - 2024-0X-0X =====
+//
+//  Functionality: Reads and converts csv file.
+//
+// ===============================================
+
+
+
+#include "input.hpp"
+#include <vector>
+using namespace std;
+
+
---
+++ src/input.hpp
@@ -0,0 +1,18 @@
+// ===== Established 2024-04-08 - 2024-0X-0X =====
+//
+//  Functionality: Reads and converts csv file.
+//
+// ===============================================
+
+
+
+#ifndef input_hpp
+#define input_hpp
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+#endif
